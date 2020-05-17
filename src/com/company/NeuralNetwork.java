722ffb319cccc0afa332db762c55768f02e0c503
JoeBoiii72 <joekenyon72@gmail.com>
2020-05-17 20:01:45 +0100

    No more syntax errors

--- src/com/company/NeuralNetwork.java
+++ src/com/company/NeuralNetwork.java
@@ -1,150 +1,173 @@
 package com.company;
 
-using System.Collections.Generic;
-        using System;
-public class NeuralNetwork : IComparable<NeuralNetwork>
-{
-public int[] layers;
-public float[][] neurons;
-public float[][][] weights;
-private float fitness;
+import java.util.ArrayList;
+import java.util.Random;
 
-public NeuralNetwork(int[] layers)
+public class NeuralNetwork
+{
+    public int[] layers;
+    public ArrayList<float[]> neurons;
+    public ArrayList<ArrayList<float[]>> weights;
+    private float fitness;
+
+    public static float randFloat(float min, float max) {
+
+        Random rand = new Random();
+
+        return rand.nextFloat() * (max - min) + min;
+
+    }
+
+    public NeuralNetwork(int[] layers)
+    {
+        this.layers = new int[layers.length];
+        for (int i = 0; i < layers.length; i++)
         {
-        this.layers = new int[layers.Length];
-        for (int i = 0; i < layers.Length; i++)
-        {
-        this.layers[i] = layers[i];
+            this.layers[i] = layers[i];
         }
         InitNeurons();
         InitWeights();
+    }
+
+    public NeuralNetwork(NeuralNetwork copyNetwork)
+    {
+        this.layers = new int[copyNetwork.layers.length];
+
+        for (int i = 0; i < copyNetwork.layers.length; i++)
+        {
+            this.layers[i] = copyNetwork.layers[i];
         }
 
-public NeuralNetwork(NeuralNetwork copyNetwork)
-        {
-        this.layers = new int[copyNetwork.layers.Length];
-        for (int i = 0; i < copyNetwork.layers.Length; i++)
-        {
-        this.layers[i] = copyNetwork.layers[i];
-        }
         InitNeurons();
         InitWeights();
         CopyWeights(copyNetwork.weights);
-        }
+    }
 
-private void CopyWeights(float[][][] copyWeights)
+private void CopyWeights(ArrayList<ArrayList<float[]>> copyWeights)
+{
+    for (int i = 0; i < weights.size(); i++)
+    {
+        for (int j = 0; j < weights.get(i).size(); j++)
         {
-        for (int i = 0; i < weights.Length; i++)
-        {
-        for (int j = 0; j < weights[i].Length; j++)
-        {
-        for (int k = 0; k < weights[i][j].Length; k++)
-        {
-        weights[i][j][k] = copyWeights[i][j][k];
+            for (int k = 0; k < weights.get(i).get(j).length; k++)
+            {
+                weights.get(i).get(j)[k] = copyWeights.get(i).get(j)[k];
+            }
         }
-        }
-        }
-        }
+    }
+}
 
 private void InitNeurons()
-        {
-        List<float[]> neuronsList = new List<float[]>();
-        for (int i = 0; i < layers.Length; i++)
-        {
-        neuronsList.Add(new float[layers[i]]);
-        }
-        neurons = neuronsList.ToArray();
-        }
+{
+    ArrayList<float[]> neuronsList = new ArrayList<>();
+
+    for (int i = 0; i < layers.length; i++)
+    {
+        neuronsList.add(new float[layers[i]]);
+    }
+    neurons = neuronsList;
+}
 
 private void InitWeights()
-        {
-        List<float[][]> weightsList = new List<float[][]>();
-        for (int i = 1; i < layers.Length; i++)
-        {
-        List<float[]> layerWeightList = new List<float[]>();
+{
+    ArrayList<ArrayList<float[]>> weightsList = new ArrayList<>();
+
+    for (int i = 1; i < layers.length; i++)
+    {
+        ArrayList<float[]> layerWeightList = new ArrayList<>();
         int neuronsInPreviousLayer = layers[i - 1];
-        for (int j = 0; j < neurons[i].Length; j++)
+
+        for (int j = 0; j < neurons.get(i).length; j++)
         {
-        float[] neuronWeights = new float[neuronsInPreviousLayer];
-        for (int k = 1; k < neuronsInPreviousLayer; k++)
-        {
-        neuronWeights[k] = UnityEngine.Random.Range(-1.0f, 1.0f);
+            float[] neuronWeights = new float[neuronsInPreviousLayer];
+            for (int k = 1; k < neuronsInPreviousLayer; k++)
+            {
+                neuronWeights[k] = randFloat(-1.0f, 1.0f);
+            }
+            layerWeightList.add(neuronWeights);
         }
-        layerWeightList.Add(neuronWeights);
-        }
-        weightsList.Add(layerWeightList.ToArray());
-        }
-        weights = weightsList.ToArray();
-        }
+        weightsList.add(layerWeightList);
+    }
+    weights = weightsList;
+}
+
 public float[] FeedForward(float[] inputs)
+{
+    for (int i = 0; i < inputs.length; i++)
+    {
+        neurons.get(0)[i] = inputs[i];
+    }
+    for (int i = 1; i < layers.length; i++)
+    {
+        for (int j = 0; j < neurons.get(i).length; j++)
         {
-        for (int i = 0; i < inputs.Length; i++)
-        {
-        neurons[0][i] = inputs[i];
+            float value = 0.25f;
+            for (int k = 0; k < neurons.get(i-1).length; k++)
+            {
+                value += weights.get(i-1).get(j)[k]*neurons.get(i-1)[k];
+            }
+            neurons.get(i)[j] = (float)Math.tanh(value);
         }
-        for (int i = 1; i < layers.Length; i++)
-        {
-        for (int j = 0; j < neurons[i].Length; j++)
-        {
-        float value = 0.25f;
-        for (int k = 0; k < neurons[i - 1].Length; k++)
-        {
-        value += weights[i - 1][j][k]*neurons[i - 1][k];
-        }
-        neurons[i][j] = (float)Math.Tanh(value);
-        }
-        }
-        return neurons[neurons.Length - 1];
-        }
+    }
+    return neurons.get(neurons.size() - 1);
+}
+
 public void Crossover(NeuralNetwork parent, NeuralNetwork partner, float mutationRate)
+{
+    for (int i = 0; i < weights.size(); i++)
+    {
+        for (int j = 0; j < weights.get(i).size(); j++)
         {
-        for (int i = 0; i < weights.Length; i++)
-        {
-        for (int j = 0; j < weights[i].Length; j++)
-        {
-        for (int k = 0; k < weights[i][j].Length; k++)
-        {
-        float mutationChance = UnityEngine.Random.Range(0.0f, 1.0f);
-        if (mutationChance < mutationRate)
-        {
-        weights[i][j][k] = UnityEngine.Random.Range(-1.0f, 1.0f);
+            for (int k = 0; k < weights.get(i).get(j).length; k++)
+            {
+                float mutationChance = randFloat(0.0f, 1.0f);
+                if (mutationChance < mutationRate)
+                {
+                    weights.get(i).get(j)[k] = randFloat(-1.0f, 1.0f);
+                }
+                else
+                {
+                    float randomNumber = randFloat(0.0f, 1.0f);
+                    if (randomNumber < 0.5)
+                        weights.get(i).get(j)[k] = parent.weights.get(i).get(j)[k];
+
+                    weights.get(i).get(j)[k] = partner.weights.get(i).get(j)[k];
+                }
+            }
         }
-        else
-        {
-        float randomNumber = UnityEngine.Random.Range(0.0f, 1.0f);
-        if (randomNumber < 0.5) weights[i][j][k] = parent.weights[i][j][k];
-        weights[i][j][k] = partner.weights[i][j][k];
-        }
-        }
-        }
-        }
-        }
+    }
+}
+
 public void Randomise()
+{
+    for (int i = 0; i < weights.size(); i++)
+    {
+        for (int j = 0; j < weights.get(i).size(); j++)
         {
-        for (int i = 0; i < weights.Length; i++)
-        {
-        for (int j = 0; j < weights[i].Length; j++)
-        {
-        for (int k = 0; k < weights[i][j].Length; k++)
-        {
-        weights[i][j][k] = UnityEngine.Random.Range(-1.0f, 1.0f);
+            for (int k = 0; k < weights.get(i).get(j).length; k++)
+            {
+                weights.get(i).get(j)[k] = randFloat(-1.0f, 1.0f);
+            }
         }
-        }
-        }
-        }
+    }
+}
+
 public void SetFitness(float fit)
-        {
-        fitness = fit;
-        }
+{
+    fitness = fit;
+}
+
 public float GetFitness()
-        {
-        return fitness;
-        }
+{
+    return fitness;
+}
+
 public int CompareTo(NeuralNetwork other)
-        {
-        if (other == null) return 1;
-        if (fitness > other.fitness) return 1;
-        else if (fitness < other.fitness) return -1;
-        else return 0;
-        }
-        }
+{
+    if (other == null) return 1;
+    if (fitness > other.fitness) return 1;
+    else if (fitness < other.fitness) return -1;
+    else return 0;
+}
+
+}
