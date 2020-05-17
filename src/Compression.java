f1ee589d7dae8b851308c4ef70850c3fd950f243
JoeBoiii72 <joekenyon72@gmail.com>
2020-05-17 20:27:04 +0100

    Created Pied Piper Compression

---
+++ src/Compression.java
@@ -0,0 +1,7 @@
+public class Compression
+{
+    public Compression()
+    {
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+class DisplayImage
+{
+    public static void display(BufferedImage img)
+    {
+        ImageIcon icon=new ImageIcon(img);
+        JFrame frame=new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(200,300);
+        JLabel lbl=new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args)
     {
-	    System.out.println("ETHAN");
+	    BufferedImage img = new BufferedImage(100,100,1);
+	    DisplayImage.display(img);
     }
 }
